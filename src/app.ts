fc23bdfa38c057d2c4e799889014a1a831e8925d
Bill Barnes <bill@billba-mbp.guest.corp.microsoft.com>
2018-03-13 15:23:55 -0700

    added demo for extending context into your own object

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { ConsoleAdapter, MemoryStorage, ConversationState } from 'botbuilder';
+import { ConsoleAdapter, MemoryStorage, ConversationState, BotContext } from 'botbuilder';
 
 interface MyState {
     count: number;
@@ -7,15 +7,51 @@ interface MyState {
 // Add conversation state middleware
 const conversationState = new ConversationState<MyState>(new MemoryStorage());
 
-const adapter = new ConsoleAdapter()
-    .use(conversationState);
+const bot = new ConsoleAdapter()
 
-adapter.listen(async (context) => {
-    if (context.request.type === 'message') {
-        const state = conversationState.get(context)!;
-        const count = state.count === undefined ? 0 : state.count + 1;
-        await context.sendActivity(`${count}: You said "${context.request.text}"`);
+class AppContext <State> extends BotContext {
+
+    // define the properties and methods to add to BotContext
+    state!: State;
+    reply(text: string) {
+        return this.sendActivity(text);
+    }
+
+    // this is a nasty piece of work that will go away
+    private constructor(
+        context: BotContext,
+    ) {
+        super(context.adapter, context.request);
+        Object.assign(this, context);
+    }
+
+    // "from" adds any properties or methods that depend on arguments or async calls or both
+    static async from <State = any> (
+        context: BotContext,
+        conversationState: ConversationState<State>,
+    ): Promise<AppContext<State>> {
+        const appContext = new AppContext<State>(context);
+        appContext.state = await conversationState.read(context);
+        return appContext;
+    }
+}
+
+bot
+    .use(conversationState)
+    .use(async (context, next) => {
+        const state = await conversationState.read(context);
+        // use state
+        await next();
+        // do stuff after child
+    })
+
+bot.listen(async (context) => {
+    const app = await AppContext.from(context, conversationState);
+    if (app.request.type === 'message') {
+        const count = app.state.count === undefined ? 0 : app.state.count + 1;
+        await app.reply(`${count}: You said "${app.request.text}"`);
     } else {
-        await context.sendActivity(`[${context.request.type} event detected]`);
+        await app.reply(`[${app.request.type} event detected]`);
     }
 });
+
